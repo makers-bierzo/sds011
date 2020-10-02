163ebfc90ce1ab185f28b4db9eedc728ebb00eb8
Aitor González Fernández <info@aitorgf.com>
2020-10-02 19:49:39 +0200

    ADD: Some documentation

--- mode.go
+++ mode.go
@@ -3,6 +3,8 @@ package sds011
 type Mode byte
 
 const (
+	// The sensor read values asynchronously that you can listen setting a channel with `.OnQuery()`.
 	ActiveMode Mode = 0
+	// The sensor only read values when a `.Query()` is executed explicitly.
 	QueryMode  Mode = 1
 )
